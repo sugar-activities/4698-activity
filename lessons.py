8e16dbb833c5443aba6d4e09578e6653f54f5710
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-04-21 01:47:13 -0400

    Initial commit

---
+++ lessons.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+from gettext import gettext as _
+import glib
+
+LESSONS = (
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('El origen de la historia: empieza aquí')) + \
+        '</span>' + \
+        '\n\n',
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 1')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Regla #1: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Existen reglas para el Juego de la Vida')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave: ')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • El juego de la vida está compuesto de los mismos elementos que otros juegos: reglas, metas, jugadores y retos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Para jugar el juego de la vida correctamente, primero necesitamos identificar los elementos y seguir las reglas.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Todos estamos jugando en el Juego de la Vida y, por esto, las mismas reglas aplican para todos.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Palabras nuevas:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Reglas: Conjunto de instrucciones que guiarán nuestro comportamiento y nos mantendrán en el camino correcto para ganar el Juego de la Vida.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • ¿Cuáles son algunas de las reglas que conoces en tu vida y cómo te ayudan? Piensa en reglas que tienes en tu casa, escuela, tu vecindario y otros lugares que visitas.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Describe cómo piensas que sería el mundo si no tuviéramos reglas. ¿Por qué crees que sería así?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • ¿Cuáles reglas son las más difíciles de seguir para ti? ¿Por qué?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 2')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Regla #2: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Lo que verdaderamente queremos es plenitud espiritual.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Las cosas físicas son temporales y se pueden percibir con los cinco sentidos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • La plenitud espiritual es duradera e intangible.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • La plenitud espiritual es nuestra meta principal en la vida.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Nuestras metas físicas pueden cambiar a través de nuestra vida, mientras nuestro deseo de plenitud espiritual se mantiene siempre constante.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Ganar en el Juego de la Vida es lo mismo que estar feliz y colmado de plenitud espiritual.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Palabras nuevas:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Plenitud espiritual: Todos los sentimientos que verdaderamente queremos (felicidad, amor, seguridad, etc.) y que no pueden ser medidos, pesados, contados o percibidos con los cinco sentidos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Cosas físicas: Objetos que pueden ser medidos, contados, pesados o percibidos con nuestros cinco sentidos; cosas que no duran para siempre.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Meta: Algo que logramos o un resultado que deseamos.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Llena la tabla en la página #14 de tu Diario con las cosas físicas que quieres y la plenitud espiritual que recibes de ellas.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Mira la tabla y contesta las preguntas a continuación:')) + \
+        '\n' + \
+        glib.markup_escape_text(_('      ‣ ¿Quieres las mismas cosas físicas hoy como las que querías hace una semana, hace un mes, hace un año o cuando eras un bebé? ¿Por qué?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('      ‣ ¿Quieres la misma plenitud espiritual hoy como hace una semana, un mes, un año o cuando eras un bebé? ¿Por qué?')),
+
+)
---
+++ videos.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+VIDEOS = (
+    ("0.ogv",
+     "El origen de la historia: empieza aquí",
+     "Esta es la historia de un joven llamado Ari quien, al igual que muchos niños y niñas, es idealista, con los ojos muy abiertos y un poco sarcástico. Este sarcasmo lo mete en problemas cuando visita la tienda de juguetes del señor Guía. El señor Guía ve en Ari el deseo de aprender y le da un juego especial llamado El Juego de la Vida. Ari regresa a casa para jugar el juego y se encuentra atrapado en un universo alternativo donde tiene que aprender las reglas del juego para volver a casa a su propia vida. Por suerte, a lo largo del recorrido interactivo de Ari, el señor Guía lo acompaña. Junto con tus niños y niñas Ari descubre que todo lo que está aprendiendo en el juego le ayuda en la vida real también.",
+     ),
+    ("1.ogv",
+     "La vida es como un juego",
+     'Cuando nuestros hijos se sienten frustrados e impotentes, podemos empoderarlos proporcionando herramientas para descubrir su verdadero potencial. Esta lección presenta la analogía de SFK y el tema central, "La vida es como un juego".  Les guiará a enseñar a sus hijos la importancia de tener reglas y que conocer y seguir las reglas dará lugar a una vida más feliz y más plena.',
+     ),
+    ("2.ogv",
+     "Plenitud",
+     "¡Más  juguetes! ¡Dulces extra! ¡Los nuevos juegos de video! Parece que no importa lo mucho que reciben nuestros hijos, siempre quieren más. En esta lección, descubre lo que realmente motiva sus deseos en el Juego de la Vida.",
+    ),
+)
